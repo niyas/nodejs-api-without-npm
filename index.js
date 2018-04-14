67f16914d190d6ce3efcb91812ca6a6da2152734
Mohammed Niyas <Mohammedniyas@MSSPLLAP066.meritgroup.com>
2018-04-14 15:16:23 +0100

    added workers library

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
  // Dependencies
  var server = require('./lib/server');
- var worker = require('./lib/workers');
+ var workers = require('./lib/workers');
 
  // Declare the app
  var app = {};
@@ -15,7 +15,7 @@
     server.init();
 
     // Strat the worker
-    worker.init();
+    workers.init();
  };
 
  // Execute
